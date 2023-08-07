7e6a14eddd05ac5e8f998fd166f2c4745b1d793b
arnu515test <>
2023-08-07 01:49:36 -0700

    first commit

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const cors = require("cors");
 const socketIO = require("socket.io");
-const {createServer} = require("http");
+const { createServer } = require("http");
 
 const app = express();
 app.use(cors());
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 
@@ -14,14 +14,14 @@ const io = socketIO(server);
 const rooms = {}
 
 app.get("/w3.css", (_, res) => {
-  res.sendFile("w3.css", {root: __dirname})
+  res.sendFile("w3.css", { root: __dirname })
 });
 
 app.get("/", (_, res) => res.render("index"))
 app.get("/room", (_, res) => res.render("index"))
 
 app.post("/room", (req, res) => {
-  const {roomid} = req.body;
+  const { roomid } = req.body;
   if (!roomid) {
     res.send("Invalid room id");
     return;
@@ -47,13 +47,13 @@ app.post("/room", (req, res) => {
 })
 
 app.get("/room/:roomId", (req, res) => {
-  const {roomId} = req.params;
+  const { roomId } = req.params;
   if (!roomId || !Array.isArray(rooms[roomId])) {
     res.redirect("/")
     return;
   }
 
-  res.render("room", {roomId})
+  res.render("room", { roomId })
 })
 
 io.on("connection", socket => {
@@ -66,13 +66,13 @@ io.on("connection", socket => {
 
   socket.on("join-room", (roomId) => {
     if (!rooms[roomId]) {
-      socket.emit("err", {redir: true, event: "join-room", message: "Invalid room id"});
+      socket.emit("err", { redir: true, event: "join-room", message: "Invalid room id" });
       return;
     }
 
     const room = rooms[roomId];
     if (room.length >= 2) {
-      socket.emit("err", {redir: true, event: "join-room", message: "Room is full"});
+      socket.emit("err", { redir: true, event: "join-room", message: "Room is full" });
       return;
     }
 
@@ -81,17 +81,17 @@ io.on("connection", socket => {
       const opp = room[0];
       socket.roomId = roomId;
       socket.isOwner = false;
-      socket.emit("cb:join-room", {roomId, isOwner: false})
-      socket.emit("new-member", {sid: opp.id})
+      socket.emit("cb:join-room", { roomId, isOwner: false })
+      socket.emit("new-member", { sid: opp.id })
       rooms[roomId] = [opp, socket];
-      opp.emit("new-member", {sid: socket.id})
+      opp.emit("new-member", { sid: socket.id })
       return;
     } else if (room.length === 0) {
       console.log("Joined room: " + roomId)
       rooms[roomId] = [socket];
       socket.roomId = roomId;
       socket.isOwner = false;
-      socket.emit("cb:join-room", {roomId, isOwner: true})
+      socket.emit("cb:join-room", { roomId, isOwner: true })
       return;
     }
   })
@@ -100,21 +100,21 @@ io.on("connection", socket => {
     console.log(`${socket.id} is signalling ${data}`)
     const room = rooms[socket.roomId];
     if (!Array.isArray(room)) {
-      socket.emit("err", {redir: true, event: "signal", message: "Not in a room"});
+      socket.emit("err", { redir: true, event: "signal", message: "Not in a room" });
       return;
     }
 
     if (room.length !== 2) {
-      socket.emit("err", {redir: true, event: "signal", message: "Opp not there"});
+      socket.emit("err", { redir: true, event: "signal", message: "Opp not there" });
       return;
     }
 
     const opp = room.find(i => i.id !== socket.id);
     try {
-      opp.emit("signal", {data: JSON.parse(data)})
+      opp.emit("signal", { data: JSON.parse(data) })
       console.log(`${socket.id} signalled to ${opp.id}`)
     } catch {
-      socket.emit("err", {redir: true, event: "signal", message: "Invalid signal"});
+      socket.emit("err", { redir: true, event: "signal", message: "Invalid signal" });
       return;
     }
   })
@@ -125,12 +125,12 @@ io.on("connection", socket => {
     const room = rooms[socket.roomId];
     if (!Array.isArray(room)) return
     if (room.length === 2) {
-        const opp = room.find(i => i.id !== socket.id);
-        if (opp) {
-          opp.emit("member-left", {sid: socket.id});
-          opp.isOwner = true
-          rooms[socket.roomId] = [opp];
-        }
+      const opp = room.find(i => i.id !== socket.id);
+      if (opp) {
+        opp.emit("member-left", { sid: socket.id });
+        opp.isOwner = true
+        rooms[socket.roomId] = [opp];
+      }
     } else {
       delete rooms[socket.roomId];
     }
